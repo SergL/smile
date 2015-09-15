650da769bc4716ace189aed2bea910a749de9090
s.litvinov <s.litvinov@favorittoto.com.ua>
2015-09-15 18:59:39 +0300

    Testing data

--- page.php
+++ page.php
@@ -1,3 +1,63 @@
+<?php
+
+//$fp = fopen("counter.txt", "a+"); // Открываем файл в режиме записи
+//$mytext = "Это строку необходимо нам записать\r\n"; // Исходная строка$data
+//$data= print_r($_REQUEST,true);
+//$test = fwrite($fp, $data); // Запись в файл
+//die();
+//if ($test) echo 'Данные в файл успешно занесены.';
+//else echo 'Ошибка при записи в файл.';
+//fclose($fp); //Закрытие файла
+
+if (isset($_POST)){
+/* получатели */
+$to= "serg_l@ukr.net\n" ;
+//. ", " ; //обратите внимание на запятую
+//$to .= "Kelly <kelly@example.com>";
+
+/* тема/subject */
+$subject = "Smiler\n";
+
+/* сообщение */
+$message = '
+<html>
+<head>
+ <title>Birthday Reminders for August</title>
+</head>
+<body>
+<p>Here are the birthdays upcoming in August!</p>
+<table>
+ <tr>
+<th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+ </tr>
+ <tr>
+<td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+ </tr>
+ <tr>
+<td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+ </tr>
+</table>
+</body>
+</html>
+';
+
+/* Для отправки HTML-почты вы можете установить шапку Content-type. */
+$headers= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+
+/* дополнительные шапки */
+//$headers .= "From: Birthday Reminder <birthday@example.com>\r\n";
+//$headers .= "Cc: birthdayarchive@example.com\r\n";
+//$headers .= "Bcc: birthdaycheck@example.com\r\n";
+
+/* и теперь отправим из */
+$result  =mail($to, $subject, $message, $headers);
+//    $result  = mail("serg_l@ukr.net", 'smile','test');
+//    mail("serg_l@ukr.net", "Smile", 'test');
+print_r($result);
+die();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
