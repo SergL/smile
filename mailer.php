d7147e644d52156e8e881be052889d2fecb64d45
Serg L <serg_l@ukr.net>
2015-09-21 23:24:52 +0300

    Change  to

--- mailer.php
+++ mailer.php
@@ -7,25 +7,23 @@
  */
 
 
-//$fp = fopen("counter.txt", "a+"); // Открываем файл в режиме записи
-//$mytext = "Это строку необходимо нам записать\r\n"; // Исходная строка$data
-$data= print_r($_REQUEST,true);
-//$test = fwrite($fp, $data); // Запись в файл
-//die();
-//if ($test) echo 'Данные в файл успешно занесены.';
-//else echo 'Ошибка при записи в файл.';
-//fclose($fp); //Закрытие файла
+$data = print_r($_POST, true);
 
+/* Адрес отсылки почты*/
+
+//$to="serg_l@ukr.net";
+$to = "vishnyakov.vasily@gmail.com";
+
+/* Шаблон заголовка*/
 $subject_tmpl = "Запись на прием от %s\n";
-
-/* сообщение */
+/* сообщение шаблон*/
 $message_tmpl = '
 <html>
 <head>
  <title>Data form</title>
 </head>
 <body>
-Date:
+<b>Date:</b> %s<br>
 %s
 
 </body>
@@ -34,19 +32,43 @@ Date:
 
 
 /* Для отправки HTML-почты вы можете установить шапку Content-type. */
-$headers= "MIME-Version: 1.0\r\n";
+$headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-$to="serg_l@ukr.net";
+
 /* дополнительные шапки */
 //$headers .= "From: Birthday Reminder <birthday@example.com>\r\n";
-$headers .= "Cc: vishnyakov.vasily@gmail.com\r\n";
-$headers .= "Bcc: crazy_serg@ukr,net\r\n";
+//$headers .= "Cc: vishnyakov.vasily@gmail.com\r\n";
+//$headers .= "Bcc: crazy_serg@ukr,net\r\n";
 
-$subject= sprintf($subject_tmpl,$_POST['name']);
-$message= sprintf($message_tmpl,date("Y-m-d H:i:s"),$data);
-/* и теперь отправим из */
-$result  =mail($to, $subject, $message, $headers);
-//    $result  = mail("serg_l@ukr.net", 'smile','test');
-//    mail("serg_l@ukr.net", "Smile", 'test');
-//mail("serg_l@ukr.net", "$subject", $data);
+$subject = sprintf($subject_tmpl, $_POST['name']);
+$data_message = str_replace(
+    'Array',
+    '<b>Данные формы:</b><br>',
+    str_replace(
+        '(',
+        '',
+        str_replace(
+            ')',
+            '',
+            str_replace(
+                ']',
+                '',
+                str_replace(
+                    '[',
+                    '<br>',
+                    $data
+                )
+            )
+        )
+    )
+);
+$message = sprintf($message_tmpl, date("Y-m-d H:i:s"), $data_message);
+
+/* Отсылка почты, при необходимости дубликата, дублируешь строчку и меняешь  $to*/
+
+mail($to, $subject, $message, $headers);
+
+//$to = "serg_l@ukr.net";
+//mail($to, $subject, $message, $headers);
 ?>
+
